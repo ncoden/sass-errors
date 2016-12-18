9e0a7f587a7a18fdb30185605ea7da8153ecee30
Nicolas Coden <nicolas@ncoden.fr>
2016-12-18 19:09:21 +0100

    Improve generated Sass documentation
    Changes:
    - Do not show private functions
    - Show aliases
    - Rename groups

--- gulpfile.js
+++ gulpfile.js
@@ -16,5 +16,16 @@ gulp.task('docs', function () {
   return gulp.src(paths.scss.src)
     .pipe(sassdoc({
       dest: paths.docs.dest,
+      display: {
+        access: ['public'],
+        alias: true,
+        watermark: true
+      },
+      groups: {
+        'api': 'API',
+        'handler': 'Exception Handlers',
+        'helper': 'Helpers',
+        'alias': 'Aliases'
+      }
     }));
 });
